0fdab85a5fafb2a5aa41852bbbecfd890ff3143e
IllyaShramko <123illya123123r@gmail.com>
2025-05-28 21:29:08 +0300

    Added possible to choose variant of question

--- library_app/views.py
+++ library_app/views.py
@@ -12,19 +12,31 @@ def render_library():
 
 def render_create_quiz():
     questions = []
+    question = None
+    create_question = False
     if flask.request.method == 'POST':
-        question = Question(
-            name = 'Question1',
-            type = 'enter answer',
-            variant_1 = 'Variant',
-            quiz_id = int(flask.request.cookies.get("draft"))
-        )
-        try:
-            DATABASE.session.add(question)
-            DATABASE.session.commit()
-            return flask.redirect(flask.url_for('/create-quiz/'))
-        except:
-            print(Exception)
+        if flask.request.form['button'] == 'one_answer':
+            question = 'One answer'
+            create_question = True
+        else:
+            type_question = flask.request.form['type_question']
+            if type_question == 'enter_answer':
+                question = Question(
+                    name = flask.request.form['question'],
+                    type = 'enter answer',
+                    variant_1 = flask.request.form['answer1'],
+                    variant_2 = flask.request.form['answer2'],
+                    variant_3 = flask.request.form['answer3'],
+                    variant_4 = flask.request.form['answer4'],
+
+                    quiz_id = int(flask.request.cookies.get("draft"))
+                )
+                try:
+                    DATABASE.session.add(question)
+                    DATABASE.session.commit()
+                    return flask.redirect(flask.url_for('/create-quiz/'))
+                except:
+                    print(Exception)
     if flask.request.cookies.get("draft"):
         cookies = int(flask.request.cookies.get("draft"))
         quiz = Quiz.query.get(cookies)
@@ -52,6 +64,8 @@ def render_create_quiz():
         'create_quiz.html',
         username = flask_login.current_user.login,
         quiz = Quiz.query.get(ident = 1),
-        questions = questions
+        questions = questions,
+        question = question,
+        create_question = create_question
         )
             
---
+++ reports_app/__init__.py
@@ -0,0 +1,2 @@
+from .app import reports_app
+from .views import render_reports_page
